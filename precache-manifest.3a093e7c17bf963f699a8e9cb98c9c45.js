534c8798bb9d22e2ae7e592a61a567e0c1d0d4db
Songdawoon <songwoonda@knou.ac.kr>
2022-06-20 18:35:06 +0900

    Updates

---
+++ precache-manifest.3a093e7c17bf963f699a8e9cb98c9c45.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e2c89746454d97024f5e42e0473f08",
+    "url": "/Songdawoon/onepuzzle/index.html"
+  },
+  {
+    "revision": "91101d01eba283b50a52",
+    "url": "/Songdawoon/onepuzzle/static/css/2.b54b9107.chunk.css"
+  },
+  {
+    "revision": "91101d01eba283b50a52",
+    "url": "/Songdawoon/onepuzzle/static/js/2.c3a399f3.chunk.js"
+  },
+  {
+    "revision": "08ae8175acb04dde7408059a162ff07e",
+    "url": "/Songdawoon/onepuzzle/static/js/2.c3a399f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb2f1420c1e50c8183e",
+    "url": "/Songdawoon/onepuzzle/static/js/main.f472cb9a.chunk.js"
+  },
+  {
+    "revision": "698b355b5e29e303283e",
+    "url": "/Songdawoon/onepuzzle/static/js/runtime-main.61c9f6b4.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/Songdawoon/onepuzzle/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/Songdawoon/onepuzzle/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/Songdawoon/onepuzzle/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "b87a09169d6814dff6d69624d827d274",
+    "url": "/Songdawoon/onepuzzle/static/media/about-page-img.b87a0916.png"
+  },
+  {
+    "revision": "a21bda4da07ba40d36de6dd27eb72a38",
+    "url": "/Songdawoon/onepuzzle/static/media/about-sec-img.a21bda4d.png"
+  },
+  {
+    "revision": "1c69a0bf55366909c0a49da3bcdfc2fa",
+    "url": "/Songdawoon/onepuzzle/static/media/hero.1c69a0bf.png"
+  },
+  {
+    "revision": "2b23a57325256c703ffeae70e99d29fa",
+    "url": "/Songdawoon/onepuzzle/static/media/map.2b23a573.png"
+  },
+  {
+    "revision": "28a260ce758e55a0f3ba35b59edc970d",
+    "url": "/Songdawoon/onepuzzle/static/media/projectImg.28a260ce.png"
+  },
+  {
+    "revision": "91aa338a30288c66ba198d6aff1c2ca6",
+    "url": "/Songdawoon/onepuzzle/static/media/project_01.91aa338a.png"
+  },
+  {
+    "revision": "b4c17f4fe3fcbc4bb21a835680484018",
+    "url": "/Songdawoon/onepuzzle/static/media/project_02.b4c17f4f.png"
+  },
+  {
+    "revision": "62b12b26efd7f22815966e04892f0f56",
+    "url": "/Songdawoon/onepuzzle/static/media/project_03.62b12b26.png"
+  },
+  {
+    "revision": "695dfdebc82c15f7da4db0b3b4c996df",
+    "url": "/Songdawoon/onepuzzle/static/media/project_04.695dfdeb.png"
+  },
+  {
+    "revision": "d494a5b68ed28a3243c05ec6f8111dcb",
+    "url": "/Songdawoon/onepuzzle/static/media/project_05.d494a5b6.png"
+  },
+  {
+    "revision": "6def7b437da0c8ccc9ef47f4725c09d4",
+    "url": "/Songdawoon/onepuzzle/static/media/project_06.6def7b43.png"
+  },
+  {
+    "revision": "97095a6ab7b315b9ae828b74278e559f",
+    "url": "/Songdawoon/onepuzzle/static/media/project_07.97095a6a.png"
+  },
+  {
+    "revision": "67a7fc34e4f29861d84d88065b198a54",
+    "url": "/Songdawoon/onepuzzle/static/media/scroll-down-arrow.67a7fc34.svg"
+  },
+  {
+    "revision": "db8e40640abe10c11fbfd65151bf7ff4",
+    "url": "/Songdawoon/onepuzzle/static/media/social-media-arrow.db8e4064.svg"
+  }
+]);
